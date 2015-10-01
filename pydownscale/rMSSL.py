010de45cda27903a6b0af420d5b4b5e8f84061ff
tjvandal <tvandal20@gmail.com>
2015-10-01 16:49:22 -0400

    fix stuff

--- pydownscale/rMSSL.py
+++ pydownscale/rMSSL.py
@@ -8,7 +8,7 @@ class pMSSL:
     def __init__(self):
         pass
 
-    def train(self,X,y,lambd=0.1, rho=0.1):
+    def train(self,X,y,lambd=2., rho=1.):
         self.X = X
         self.y = y
         self.rho = rho
@@ -23,7 +23,6 @@ class pMSSL:
         t = 0
         costs = []
         while costdiff > .1:
-            print "iteration %i" % t
             self.W = self._w_update()
             self.Omega = self._omega_update(self.Omega)
             curr_cost, _ = self._w_cost(self.W)
@@ -35,6 +34,8 @@ class pMSSL:
                 costdiff = prevcost - curr_cost
                 prevcost = curr_cost
             t += 1
+            print "iteration %i, costdiff: %f" % (t, costdiff)
+
 
     def shrinkage_threshold(self, a, alpha):
         A = (numpy.abs(a) - alpha)
@@ -51,40 +52,61 @@ class pMSSL:
             return x+thres
 
     def softthreshold(self, X, thres):
-        return numpy.piecewise(X, [X > thres, numpy.abs(X) <= thres, X < -thres], [lambda X: X - thres, 0, lambda X: X+thres])
+        return numpy.piecewise(X, 
+            [X > thres, numpy.abs(X) <= thres, X < -thres], 
+            [lambda X: X - thres, 0, lambda X: X+thres])
 
     def _omega_update(self, Omega):
         Z = numpy.zeros(shape=(self.K, self.K))
         U = numpy.zeros(shape=(self.K, self.K))
         j = 0
         dualresid = 10e6
-        while dualresid > 1:
+        resid = []
+        while dualresid > 0.001 :
             S = self.W.T.dot(self.W)
             L, Q = numpy.linalg.eig(self.rho * (Z - U) - S)
             Omega_tilde = numpy.eye(self.K)
             numpy.fill_diagonal(Omega_tilde, (L + numpy.sqrt(L**2 + 4*self.rho))/(2*self.rho))
             Omega = Q.dot(Omega_tilde).dot(Q.T)
-            Z_prev = Z
+            Z_prev = Z #.copy()
             Z = self.softthreshold(Omega + U, self.lambd/self.rho)
             U = U + Omega - Z
             dualresid = numpy.linalg.norm(self.rho * self.X.T.dot(self.y).dot(Z - Z_prev), 2)
-
+           # dualresid = numpy.linalg.norm(self.rho * (Z - Z_prev), 2)
+            if j % 100 == 0:
+                print "omega update:", j, "Dualresid:", dualresid
+            j+=1
+            resid.append(dualresid)
+            if j % 1000 == 0:
+                #pyplot.plot(resid)
+                #pyplot.draw()
+                #pyplot.show()
+                resid = []
         return Omega
 
-    def _w_update(self, tk=0.001):
-        costdiff = 1.
+    def _w_update(self, tk=1.):
+        costdiff = 10e6
         W = self.W
-        j = 1
-        while costdiff > 1:
+        j = 0
+        tk = 1/(2*numpy.linalg.norm(self.X.T.dot(self.X), 1)) # tk exits in (0, 1/||X.T*X||)
+        print "tk:", tk
+        while (costdiff > 5):
             cost, gmat = self._w_cost(W)
+
             W = self.shrinkage_threshold(W - tk*gmat, alpha=self.lambd*tk)
-            if j == 1:
-                costdiff = cost
+            if j == 0:
+                costdiff = numpy.abs(cost)
             else:
-                costdiff = costprev - cost
+                costdiff = numpy.abs(costprev - cost)
             costprev = cost
+            if j % 100 == 1:
+                print "W update:", j, ", Cost diff:", costdiff
+            if j > 1000:
+                print "Warning: W did not converge."
+                break
             j += 1
         return W
+
 
     # Lets parallelize this
     def _w_cost(self, W):
@@ -92,7 +114,7 @@ class pMSSL:
         f = (self.y-XW).T.dot((self.y-XW)) / (2*len(self.y))
         f += self.lambd*numpy.trace(W.dot(self.Omega).dot(W.T))
         gmat = (self.X.T.dot(XW) - self.X.T.dot(self.y))/len(self.y)  # the gradients
-        gmat += 2*self.lambd*W.dot(self.Omega)
+        gmat += 2*W.dot(self.Omega) # *self.lambd
         return numpy.sum(f), gmat
 
     def predict(self, X):
@@ -106,18 +128,18 @@ def test1():
     W[:,:4] += numpy.random.normal(0, 5, size=(d,1))
     W[:,5:10] += numpy.random.normal(0, 5, size=(d,1))
     X = numpy.random.uniform(size=(n,d))
-    y = X.dot(W) + numpy.random.normal(0,0.5, size=(n,k))
+    y = X.dot(W)
     mssl = pMSSL()
     mssl.train(X,y)
-    pyplot.imshow(numpy.linalg.inv(mssl.Omega))
-    #pyplot.imshow(mssl.Omega)
+    #pyplot.imshow(numpy.linalg.inv(mssl.Omega))
+    pyplot.imshow(mssl.Omega)
     pyplot.show()
 
 def climatetest():
     import time
     t0 = time.time()
-    cmip5_dir = "/Users/tj/data/cmip5/access1-3/"
-    cpc_dir = "/Users/tj/data/usa_cpc_nc/merged"
+    cmip5_dir = "/scratch/vandal.t/cmip5/access1-3/"
+    cpc_dir = "/scratch/vandal.t/merged/"
 
     # climate model data, monthly
     cmip5 = read_nc_files(cmip5_dir)
@@ -134,7 +156,6 @@ def climatetest():
     #data.normalize_monthly()
 
     # print "Data Normalized: %d" % (time.time() - t0)
-    mssl = pMSSL()
     X = data.get_X()
     lats = data.observations.lat
     lons = data.observations.lon
@@ -142,12 +163,16 @@ def climatetest():
     i = len(lons)/2
     Y = data.observations.loc[{'lat': lats[j:j+5], 'lon': lons[i:i+5]}].to_array().values.squeeze()
     Y = Y.reshape(Y.shape[0], Y.shape[1]*Y.shape[2])
-
-    mssl.train(X[:70, :5000], Y[:70])
-    yhat = mssl.predict(X[70:])
-    pyplot.plot(yhat[70:,0])
-    pyplot.plot(Y[70:, 0], color='red')
-    pyplot.show()
+    
+    mssl = pMSSL()
+    mssl.train(X[:70,:5000], Y[:70])
+    yhat = mssl.predict(X[70:,:5000])
+    #pyplot.plot(yhat[70:,0])
+    #pyplot.plot(Y[70:, 0], color='red')
+    #pyplot.show()
 
 if __name__ == "__main__":
+    #test1()
     climatetest()
+
+
