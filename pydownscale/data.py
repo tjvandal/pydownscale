54723f87fb087394c3f973844dd6c9f0dc4e75e1
tvandal <tvandal20@gmail.com>
2015-11-17 22:56:51 -0500

    lowres input can now have different dimensions

--- pydownscale/data.py
+++ pydownscale/data.py
@@ -5,56 +5,81 @@ import numpy
 import pandas
 from scipy.misc import factorial
 
+'''
+Lets let ncep be a list of xray datasets where the time axis matches but may have different levels
+normalization is no needed at this step, atleast until training
+'''
+
 class DownscaleData:
-    def __init__(self, cmip, observations):
-        self.cmip = cmip
+    def __init__(self, ncep, observations):
+        if isinstance(ncep, xray.Dataset):
+            ncep = [ncep]
+
+        self.ncep = ncep
         self.observations = observations
+        self._check_ncep()
         self._checkindices()
         self._matchdates()
 
+    def _check_ncep(self):
+        for n in self.ncep:
+            if (not isinstance(n, xray.Dataset)) and (not isinstance(n, xray.DataArray)):
+                raise TypeError("Ncep should be a list of xray datasets or dataarrays.")
+
     # can we make this dynamic in the future?
     def _checkindices(self):
-        if not 'time' in self.cmip.dims:
-            raise IndexError("time should be a dimension of cmip")
+        times = []
+        for d in self.ncep:
+            if not 'time' in d.dims:
+                raise IndexError("time should be a dimension of cmip")
+            times.append(d.time.values)
+
+        times = numpy.array(times)
+        if numpy.sum(times[0] - times) != 0:
+            raise IndexError("All times in lowres data should be identical.")
 
         if not 'time' in self.observations.dims:
             raise IndexError("time should be a dimension of observations")
 
     # Line up datasets so that the times match.  Exclude observations outside of the timeframe.
     def _matchdates(self):
-        mintime = max(self.cmip.time.min(), self.observations.time.min())
-        maxtime = min(self.cmip.time.max(), self.observations.time.max())
+        mintime = max(self.ncep[0].time.min(), self.observations.time.min())
+        maxtime = min(self.ncep[0].time.max(), self.observations.time.max())
 
-        cmiptimes = (self.cmip.time >= mintime) & (self.cmip.time <= maxtime)
+        cmiptimes = (self.ncep[0].time >= mintime) & (self.ncep[0].time <= maxtime)
         obstimes = (self.observations.time >= mintime) & (self.observations.time <= maxtime)
 
-        self.cmip = self.cmip.loc[dict(time=self.cmip.time[cmiptimes])]
+        for j in range(len(self.ncep)):
+            self.ncep[j] = self.ncep[j].loc[dict(time=self.ncep[j].time[cmiptimes])]
+
         self.observations = self.observations.loc[dict(time=self.observations.time[obstimes])]
 
-        if (len(self.cmip.time) != len(self.observations.time)) or \
-            sum(self.cmip.time == self.observations.time) != len(self.cmip.time):
+        if (len(self.ncep[j].time) != len(self.observations.time)) or \
+            sum(self.ncep[j].time == self.observations.time) != len(self.ncep[j].time):
             raise IndexError("times do not match.  add functionality if this is not an error")
 
     def get_covariate_indices(self):
-        return self.cmip.flatten()
+        return self.ncep.flatten()
 
     def normalize_monthly(self):
         def standardize_time(x):
             z = (x - x.mean('time')) / x.std('time')
             return z
-        self.cmip = self.cmip.groupby('time.month').apply(standardize_time)
-        del self.cmip['month']  ## why is the month variable created?
+        self.ncep = self.ncep.groupby('time.month').apply(standardize_time)
+        del self.ncep['month']  ## why is the month variable created?
 
-    def get_X(self, vars=None, timedim='time'):
-        self.cmip.load()
+    def get_X(self, timedim='time'):
+        x = []
+        for d in self.ncep:
+            d.load()
+            df = d.to_array().to_dataframe()
+            levels = [var for var in df.index.names if var != timedim]
+            x.append(df.unstack(levels).values)
 
-        if vars is None:
-            df = self.cmip.to_array().to_dataframe()
-        else:
-            df = self.cmip[vars].to_array().to_dataframe()
+        x = numpy.column_stack(x)
+        print x.shape
+        return x
 
-        levels = [var for var in df.index.names if var != timedim]
-        return df.unstack(levels).values
 
     def get_y(self, location=None, timedim='time'):
         if location is not None:
@@ -99,13 +124,22 @@ class DownscaleData:
         pairs = [[d1, d2] for i1, d1 in enumerate(Y.coords[dim1].values) for i2, d2 in enumerate(Y.coords[dim2].values) if t0values[i1, i2] != -999.]
         return pairs
 
+def get_ncep_file_paths(basedir):
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(basedir):
+        for f in filenames:
+            if '.nc' in f:
+                files.append(os.path.join(dirpath, f))
+
+    return files
+
 
 def read_nc_files(dir):
     def rmheight(d):
         #del d["height"]
         return d
 
-    files = [os.path.join(dir, f) for f in os.listdir(dir) if ".nc" == f[-3:]]
+    files = get_ncep_file_paths(dir)
     if len(files) > 1:
         data = xray.open_mfdataset(files, preprocess=rmheight)
     elif len(files) == 1:
@@ -126,39 +160,79 @@ def assert_bounds(data, bounds):
         loc[b] = vals[(bounds[b][0] < vals) & (bounds[b][1] > vals)]
     return data.loc[loc]
 
-if __name__ == "__main__":
-    cmip5_dir = "/scratch/vandal.t/cmip5/historical/atm/mon/MIROC-ESM/"
-    cpc_dir = "/Users/tj/data/usa_cpc_nc/merged"
+def test_data():
+    p1 = "/scratch/vandal.t/ncep/daily/downscale-data/air/"
+    p2 = "/scratch/vandal.t/ncep/daily/downscale-data/pr_wtr/"
+    files1 = get_ncep_file_paths(p1)[2:3]
+    files2 = get_ncep_file_paths(p2)[2:3]
 
-    # climate model data, monthly
-    cmip5 = read_nc_files(cmip5_dir)
-    cmip5.load()
-    cmip5 = assert_bounds(cmip5, {'lat': [20, 35], 'lon': [100, 150]})
+    d1 = xray.open_mfdataset(files1)
+    d2 = xray.open_mfdataset(files2)
+    lowres = []
+    for d in [d1, d2]:
+        d.load()
+        d = assert_bounds(d, config.lowres_bounds)
+        if 'level' in d.dims:
+            d = d.loc[dict(level=config.nceplevels)]
 
-    cmip5.time = pandas.to_datetime(cmip5.time.values)
-    cmip5 = cmip5.resample('MS', 'time', how='mean')   ## try to not resample
+        d = d.resample('MS', 'time', how='mean')
+        lowres.append(d)
 
-    # daily data to monthly
     cpc = read_nc_files(cpc_dir)
     cpc.load()
     cpc.time = pandas.to_datetime(cpc.time.values)
-    cpc = assert_bounds(cpc, {'lat': [20, 35], 'lon': [-120, -100]})
+    cpc = assert_bounds(cpc, config.highres_bounds)
+    cpc = cpc.resample('MS', dim='time', how='mean')
+    return lowres, cpc
 
-    print "resampling cpc"
-    monthlycpc = cpc.resample('MS', dim='time', how='mean')  ## try to not resample
+def read_config_data_monthly():
+    import config
 
-    d = DownscaleData(cmip5, monthlycpc)
-    print "Normalizing"
-    d.normalize_monthly()
-    X = d.get_X(vars=['uas', 'vas', 'tasmax', 'tasmin', 'hurs'])
+    vars = os.listdir(config.ncep_dir)
+    lowres = []
+    for v in vars:
+        fv = get_ncep_file_paths(os.path.join(config.ncep_dir, v))
+        if len(fv) == 0:
+            continue
+        print v
+        d = xray.open_mfdataset(fv)
+        d = assert_bounds(d, config.lowres_bounds)
+        d.load()
+        d = d.resample('MS', 'time', how='mean')
+        lowres.append(d)
+
+    cpc = read_nc_files(config.cpc_dir)
+    cpc.load()
+    cpc.time = pandas.to_datetime(cpc.time.values)
+    cpc = assert_bounds(cpc, config.highres_bounds)
+    cpc = cpc.resample('MS', dim='time', how='mean')
+
+    return DownscaleData(lowres, cpc)
+
+
+
+if __name__ == "__main__":
+
+    read_config_data_monthly()
+    '''
+    import config
+    ncep_dir = config.ncep_dir
+    cpc_dir = config.cpc_dir
+
+    ncep, cpc = test_data()
+
+    d = DownscaleData(ncep, cpc)
+    X = d.get_X()
+
     print d.location_pairs('lat', 'lon')
 
-    lt = d.cmip['lat'].values[0]
-    ln = d.cmip['lon'].values[0]
-    prcp = d.cmip.loc[{'lat':  lt, 'lon': ln}]
+    lt = d.ncep[0]['lat'].values[0]
+    ln = d.ncep[0]['lon'].values[0]
+    prcp = d.ncep[0].loc[{'lat':  lt, 'lon': ln}]
 
     from matplotlib import pyplot
     import seaborn
 
-    pyplot.hist(prcp['tas'].values)
+    pyplot.hist(X.flatten())
     pyplot.show()
+    '''
