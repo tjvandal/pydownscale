a6c90daf1efcd368007629ef6daa8328576b84ae
tjvandal <tvandal20@gmail.com>
2016-01-19 11:56:28 -0500

    allow for more general datasets

--- pydownscale/data.py
+++ pydownscale/data.py
@@ -6,131 +6,135 @@ import pandas
 from scipy.misc import factorial
 
 '''
-Lets let ncep be a list of xray datasets where the time axis matches but may have different levels
+Lets let reanalysis be a list of xray datasets where the time axis matches but may have different levels
 normalization is no needed at this step, atleast until training
 '''
 
 class DownscaleData:
-    def __init__(self, ncep, observations):
-        if isinstance(ncep, xray.Dataset):
-            ncep = [ncep]
+	def __init__(self, reanalysis, observations):
+		if isinstance(reanalysis, xray.Dataset):
+			reanalysis = [reanalysis]
 
-        self.ncep = ncep
-        self.observations = observations
-        self._check_ncep()
-        self._checkindices()
-        self._matchdates()
+		self.reanalysis = reanalysis
+		self.observations = observations
+		self._check_reanalysis()
+		self._checkindices()
+		self._matchdates()
 
-    def _check_ncep(self):
-        for n in self.ncep:
-            if (not isinstance(n, xray.Dataset)) and (not isinstance(n, xray.DataArray)):
-                raise TypeError("Ncep should be a list of xray datasets or dataarrays.")
+	def _check_reanalysis(self):
+		for _, n in self.reanalysis.iteritems():
+			if (not isinstance(n, xray.Dataset)) and (not isinstance(n, xray.DataArray)):
+				raise TypeError("Ncep should be a list of xray datasets or dataarrays.")
 
-    # can we make this dynamic in the future?
-    def _checkindices(self):
-        times = []
-        for d in self.ncep:
-            if not 'time' in d.dims:
-                raise IndexError("time should be a dimension of cmip")
-            times.append(d.time.values)
+	# can we make this dynamic in the future?
+	def _checkindices(self):
+		times = []
+		for _, d in self.reanalysis.iteritems():
+			if not 'time' in d.dims:
+				raise IndexError("time should be a dimension of cmip")
+			times.append(d.time.values)
 
-        times = numpy.array(times)
-        print times
-        print numpy.sum(times[0] - times) 
-        print type(numpy.sum(times[0] - times))
-        print numpy.sum(times[0] - times).item()
-        if numpy.sum(times[0] - times).item() != 0:
-            raise IndexError("All times in lowres data should be identical.")
+		times = numpy.array(times)
+		if numpy.sum(times[0] - times).item() != 0:
+			raise IndexError("All times in lowres data should be identical.")
 
-        if not 'time' in self.observations.dims:
-            raise IndexError("time should be a dimension of observations")
+		if not 'time' in self.observations.dims:
+			raise IndexError("time should be a dimension of observations")
 
-    # Line up datasets so that the times match.  Exclude observations outside of the timeframe.
-    def _matchdates(self):
-        mintime = max(self.ncep[0].time.min(), self.observations.time.min())
-        maxtime = min(self.ncep[0].time.max(), self.observations.time.max())
+	# Line up datasets so that the times match.  Exclude observations outside of the timeframe.
+	def _matchdates(self):
+		key1 = self.reanalysis.keys()[0]
+		mintime = max(self.reanalysis[key1].time.min(), self.observations.time.min())
+		maxtime = min(self.reanalysis[key1].time.max(), self.observations.time.max())
 
-        cmiptimes = (self.ncep[0].time >= mintime) & (self.ncep[0].time <= maxtime)
-        obstimes = (self.observations.time >= mintime) & (self.observations.time <= maxtime)
+		cmiptimes = (self.reanalysis[key1].time >= mintime) & (self.reanalysis[key1].time <= maxtime)
+		obstimes = (self.observations.time >= mintime) & (self.observations.time <= maxtime)
 
-        for j in range(len(self.ncep)):
-            self.ncep[j] = self.ncep[j].loc[dict(time=self.ncep[j].time[cmiptimes])]
+		for key in self.reanalysis:
+			self.reanalysis[key] = self.reanalysis[key].loc[dict(time=self.reanalysis[key].time[cmiptimes])]
 
-        self.observations = self.observations.loc[dict(time=self.observations.time[obstimes])]
+		self.observations = self.observations.loc[dict(time=self.observations.time[obstimes])]
 
-        if (len(self.ncep[j].time) != len(self.observations.time)) or \
-            	sum(self.ncep[j].time == self.observations.time) != len(self.ncep[j].time):
-            	print self.ncep[j].time, self.observations.time
-		raise IndexError("times do not match.  add functionality if this is not an error")
+		if (len(self.reanalysis[key1].time) != len(self.observations.time)) or \
+				sum(self.reanalysis[key1].time == self.observations.time) != len(self.reanalysis[key1].time):
+				print self.reanalysis[key1].time, self.observations.time
+				raise IndexError("times do not match.  add functionality if this is not an error")
 
-    def get_covariate_indices(self):
-        return self.ncep.flatten()
+	def get_X(self, timedim='time'):
+		import config
+		x = []
+		for var in config.reanalysisvars:
+			self.reanalysis[var].load()
+			df = self.reanalysis[var].to_array().to_dataframe()
+			levels = sorted([v for v in df.index.names if v != timedim])
+			x.append(df.unstack(levels).values)
 
-    def normalize_monthly(self):
-        def standardize_time(x):
-            z = (x - x.mean('time')) / x.std('time')
-            return z
-        self.ncep = self.ncep.groupby('time.month').apply(standardize_time)
-        del self.ncep['month']  ## why is the month variable created?
-
-    def get_X(self, timedim='time'):
-        x = []
-        for d in self.ncep:
-            d.load()
-            df = d.to_array().to_dataframe()
-            levels = [var for var in df.index.names if var != timedim]
-            x.append(df.unstack(levels).values)
-
-        x = numpy.column_stack(x)
-        return x
+		x = numpy.column_stack(x)
+		return x
 
 
-    def get_y(self, location=None, timedim='time'):
-        if location is not None:
-            y = self.observations.loc[location].to_array().values.squeeze()
-        else:
-            y = self.observations.to_array().to_dataframe()
-            levels = [var for var in y.index.names if var != timedim]
-            y = y.unstack(levels)
-            location = y.columns.to_series()
-            y = y.values
-            cols = y[0,:] != -999.
-            y = y[:, cols]
-            location = location[cols]
-        return y, location
+	def get_y(self, location=None, timedim='time'):
+		if location is not None:
+			y = self.observations.loc[location].to_array().values.squeeze()
+		else:
+			y = self.observations.to_array().to_dataframe()
+			levels = [var for var in y.index.names if var != timedim]
+			y = y.unstack(levels)
+			location = y.columns.to_series()
+			y = y.values
+			cols = y[0,:] != -999.
+			y = y[:, cols]
+			location = location[cols]
+		return y, location
 
-    def location_pairs(self, dim1, dim2):
-        Y = self.observations.to_array()
-        if (dim1 not in Y.dims) or (dim2 not in Y.dims):
-            raise IndexError("dim1=%s and dim2=%s are not in observations." % (dim1, dim2))
-        if len(Y.dims) != 4:
-            raise ValueError("There should be 4 dimensions with only 1 variable.")
+	def location_pairs(self, dim1, dim2):
+		Y = self.observations.to_array()
+		if (dim1 not in Y.dims) or (dim2 not in Y.dims):
+			raise IndexError("dim1=%s and dim2=%s are not in observations." % (dim1, dim2))
+		if len(Y.dims) != 4:
+			raise ValueError("There should be 4 dimensions with only 1 variable.")
 
-        t0 = Y['time'].values[0]
-        t0array = self.observations.loc[dict(time=t0)].to_array()
+		t0 = Y['time'].values[0]
+		t0array = self.observations.loc[dict(time=t0)].to_array()
 
-        # check which dimension axes
-        dims = t0array.dims
-        dim1_axis = [j for j in range(len(dims)) if dims[j] == dim1][0]
-        dim2_axis = [j for j in range(len(dims)) if dims[j] == dim2][0]
-        t0values = t0array.values
-        if dim1_axis > dim2_axis:
-            t0values = numpy.swapaxes(t0values, dim1_axis, dim2_axis)
+		# check which dimension axes
+		dims = t0array.dims
+		dim1_axis = [j for j in range(len(dims)) if dims[j] == dim1][0]
+		dim2_axis = [j for j in range(len(dims)) if dims[j] == dim2][0]
+		t0values = t0array.values
+		if dim1_axis > dim2_axis:
+			t0values = numpy.swapaxes(t0values, dim1_axis, dim2_axis)
 
-        t0values = t0values.squeeze()
+		t0values = t0values.squeeze()
 
-        if isinstance(t0values, float):
-            t0values = numpy.array(t0values)
-            t0values = t0values[:, numpy.newaxis]
-        elif len(Y.coords[dim1].values) == 1:
-            t0values = t0values[numpy.newaxis, :]
-        elif len(Y.coords[dim2].values) == 1:
-            t0values = t0values[:, numpy.newaxis]
+		if isinstance(t0values, float):
+			t0values = numpy.array(t0values)
+			t0values = t0values[:, numpy.newaxis]
+		elif len(Y.coords[dim1].values) == 1:
+			t0values = t0values[numpy.newaxis, :]
+		elif len(Y.coords[dim2].values) == 1:
+			t0values = t0values[:, numpy.newaxis]
 
-        pairs = [[d1, d2] for i1, d1 in enumerate(Y.coords[dim1].values) for i2, d2 in enumerate(Y.coords[dim2].values) if t0values[i1, i2] not in (-999., numpy.nan)]
-        return pairs
+		pairs = [[d1, d2] for i1, d1 in enumerate(Y.coords[dim1].values) for i2, d2 in enumerate(Y.coords[dim2].values) if t0values[i1, i2] not in (-999., numpy.nan)]
+		return pairs
 
-def get_ncep_file_paths(basedir):
+class GCMData:
+	def __init__(self, data):
+		self.data = data		
+
+	def get_X(self, timedim='time'):
+		import config
+		x = []
+		for var in config.gcmvars:
+			self.data[var].load()
+			df = self.data[var].to_array().to_dataframe()
+			levels = sorted([v for v in df.index.names if v != timedim])
+			x.append(df.unstack(levels).values)
+
+		x = numpy.column_stack(x)
+		return x
+
+def get_reanalysis_file_paths(basedir):
     files = []
     for (dirpath, dirnames, filenames) in os.walk(basedir):
         for f in filenames:
@@ -139,38 +143,48 @@ def get_ncep_file_paths(basedir):
 
     return sorted(files)
 
-
-def read_nc_files(dir):
+def read_nc_files(dir, bounds=None):
     def rmheight(d):
         #del d["height"]
         return d
 
-    files = get_ncep_file_paths(dir)
+    files = get_reanalysis_file_paths(dir)
     if len(files) > 1:
-        data = xray.open_mfdataset(files, preprocess=rmheight)
+        print files[32:35]
+        data = xray.open_mfdataset(files[30:33], preprocess=lambda d: assert_bounds(d, bounds))
     elif len(files) == 1:
         data = xray.open_dataset(files[0])
     else:
         raise IOError("There are no .nc files in that directory.")
     return data
 
-def assert_bounds(data, bounds):
+def assert_bounds(data, bounds=None):
     '''
     :param data: Should be an xray dataset
     :param bounds: dictionary of bounds {'lat': [l1 ,l2], 'lon': [l3, l4]}
     :return: xray dataset bounded by the bounds
     '''
+    def convert_lon(val):
+        return (val + 360) % 360
+
     loc = {}
+    if bounds is None:
+        return data
+
     for b in bounds:
         vals = data[b].values
+        if b in ('lon', 'longitude'):
+            data[b] = convert_lon(vals)
+            vals = data[b].values
+
         loc[b] = vals[(bounds[b][0] < vals) & (bounds[b][1] > vals)]
     return data.loc[loc]
 
 def test_data():
-    p1 = "/scratch/vandal.t/ncep/daily/downscale-data/air/"
-    p2 = "/scratch/vandal.t/ncep/daily/downscale-data/pr_wtr/"
-    files1 = get_ncep_file_paths(p1)[2:3]
-    files2 = get_ncep_file_paths(p2)[2:3]
+    p1 = "/scratch/vandal.t/reanalysis/daily/downscale-data/air/"
+    p2 = "/scratch/vandal.t/reanalysis/daily/downscale-data/pr_wtr/"
+    files1 = get_reanalysis_file_paths(p1)[2:3]
+    files2 = get_reanalysis_file_paths(p2)[2:3]
 
     d1 = xray.open_mfdataset(files1)
     d2 = xray.open_mfdataset(files2)
@@ -179,78 +193,105 @@ def test_data():
         d.load()
         d = assert_bounds(d, config.lowres_bounds)
         if 'level' in d.dims:
-            d = d.loc[dict(level=config.nceplevels)]
+            d = d.loc[dict(level=config.reanalysislevels)]
 
         d = d.resample('MS', 'time', how='mean')
         lowres.append(d)
 
-    cpc = read_nc_files(cpc_dir)
-    cpc.load()
-    cpc.time = pandas.to_datetime(cpc.time.values)
-    cpc = assert_bounds(cpc, config.highres_bounds)
-    cpc = cpc.resample('MS', dim='time', how='mean')
-    return lowres, cpc
+    obs = read_nc_files(obs_dir)
+    obs.load()
+    obs.time = pandas.to_datetime(obs.time.values)
+    obs = assert_bounds(obs, config.highres_bounds)
+    obs = obs.resample('MS', dim='time', how='mean')
+    return lowres, obs
 
-def read_config_data_monthly():
+def read_lowres_data(how='MS', which='reanalysis'):
     import config
+    if which == 'reanalysis':
+        vars = config.reanalysisvars
+        basedir = config.reanalysis_dir
+    elif which == 'gcm':
+        vars = config.gcmvars
+        basedir = config.gcm_dir
 
-    vars = os.listdir(config.ncep_dir)
-    lowres = []
-    for v in vars[:2]:
-        fv = get_ncep_file_paths(os.path.join(config.ncep_dir, v))
+    lowres = {} 
+    for v in vars:
+        fv = get_reanalysis_file_paths(os.path.join(basedir, v))
         if len(fv) == 0:
             continue
-        print v
-        d = xray.open_mfdataset(fv)
-        print "Loading"
+
+        d = xray.open_mfdataset(fv[:3], preprocess=lambda d: assert_bounds(d, config.lowres_bounds))
+        print "loading"
         d.load()
-        print "Loaded"
-        d = assert_bounds(d, config.lowres_bounds)
-        if 'level' in d.dims:
-            levels = [l for l in config.nceplevels if l in d.level.values]
-	    if len(levels) > 0:
-	    	d = d.loc[dict(level=levels)]
+        if 'plev' in d.dims:
+            levels = [l*100. for l in config.reanalysislevels if l*100. in d.plev.values]
+            if len(levels) > 0:
+                d = d.loc[dict(plev=levels)]
 
-        d = d.resample('MS', 'time', how='mean')
-        print "Sampled"
-        lowres.append(d)
+        elif 'level' in d.dims:
+            levels = [l for l in config.reanalysislevels if l in d.level.values]
 
-    cpc = read_nc_files(config.cpc_dir)
-    cpc.load()
-    cpc.time = pandas.to_datetime(cpc.time.values)
-    cpc = assert_bounds(cpc, config.highres_bounds)
-    cpc = cpc.resample('MS', dim='time', how='mean')
+            if len(levels) > 0:
+                d = d.loc[dict(level=levels)]
+        print d.dims
+        d = d.resample(how, 'time', how='mean')
+        lowres[v] = d
+    return lowres
 
-    return DownscaleData(lowres, cpc)
-
-
+def read_obs(how='MS'):
+    import config
+    obs = read_nc_files(config.obs_dir, config.highres_bounds)
+    obs.load()
+    obs.time = pandas.to_datetime(obs.time.values)
+    obs = obs.resample('MS', dim='time', how='mean')
+    return obs
 
 if __name__ == "__main__":
     import config
     import pickle
 
-    D = read_config_data_monthly()
+    print "reading reanalysis"
+    reanalysis = read_lowres_data(which='reanalysis', how='MS')
+    print "reading observations"
+    obs = read_obs(how='obs')
+    D = DownscaleData(reanalysis, obs)
     X = D.get_X()
     print "Shape of X:", X.shape
     fname = "monthly_%i_%i.pkl" % (X.shape[0], X.shape[1])
     f = os.path.join(config.save_dir, "DownscaleData", fname)
     pickle.dump(D, open(f, 'w'))
     print "Saved for file: %s" % f
+
+    gcm = read_lowres_data(which='gcm', how='MS')
+    G = GCMData(gcm)
+    GX = G.get_X()
+    print "SHAPE OF GCM X:", GX.shape
+
+    '''
+    Daily = read_config_data_daily()
+    X = Daily.get_X()
+    print "Shape of X:", X.shape
+    fname = "daily_%i_%i.pkl" % (X.shape[0], X.shape[1])
+    f = os.path.join(config.save_dir, "DownscaleData", fname)
+    pickle.dump(Daily, open(f, 'w'))
+    print "Saved for file: %s" % f
+    '''
+
     '''
     import config
-    ncep_dir = config.ncep_dir
-    cpc_dir = config.cpc_dir
+    reanalysis_dir = config.reanalysis_dir
+    obs_dir = config.obs_dir
 
-    ncep, cpc = test_data()
+    reanalysis, obs = test_data()
 
-    d = DownscaleData(ncep, cpc)
+    d = DownscaleData(reanalysis, obs)
     X = d.get_X()
 
     print d.location_pairs('lat', 'lon')
 
-    lt = d.ncep[0]['lat'].values[0]
-    ln = d.ncep[0]['lon'].values[0]
-    prcp = d.ncep[0].loc[{'lat':  lt, 'lon': ln}]
+    lt = d.reanalysis[0]['lat'].values[0]
+    ln = d.reanalysis[0]['lon'].values[0]
+    prcp = d.reanalysis[0].loc[{'lat':  lt, 'lon': ln}]
 
     from matplotlib import pyplot
     import seaborn
--- scripts/merra_preprocess.py
+++ scripts/merra_preprocess.py
@@ -18,9 +18,12 @@ rank = comm.Get_rank()
 base_dir = "/gss_gpfs_scratch/vandal.t/merra_2/"
 raw_dir = os.path.join(base_dir, "6hour", "raw")
 remap_dir = os.path.join(base_dir, "6hour", "remap")
+surface_raw_dir = os.path.join(base_dir, "6hour", "M2I1NXASM.5.12.4")
+
 
 remap = "/home/vandal.t/repos/pydownscale/scripts/merra_remap_1.0x1.25"
 vars = ['T', 'U', 'V', 'H', 'QV', 'SLP', 'PS'] 
+surface_vars = ['T2M', 'TS', 'V2M', 'U2M', 'TQV', 'TQL'] 
 months = range(1,13)
 years = range(1980, 2016)
 
@@ -30,7 +33,6 @@ if rank == 0:
     files = numpy.array_split(files, size)
 
 files = comm.scatter(files, root=0)
-
 for rawfile, f in files:
     # remap file to a higher resolution if needed
     remapfile = os.path.join(remap_dir, f.replace(".nc4", "_1.0x1.25.nc4"))
@@ -91,7 +93,11 @@ else:
 
 years = comm.scatter(years, size)
 
-
+if rank == 0:
+    for var in vars:
+        yearpath = os.path.join(base_dir, "daily_merged", var)
+        if not os.path.exists(yearpath):
+            os.mkdir(yearpath)
 
 # merge daily files into a year
 for y in years:
@@ -102,8 +108,6 @@ for y in years:
         if (os.path.exists(yearfile)): # and (os.path.getsize(yearfile) != 0):
  #           os.remove(yearfile) 
             continue
-        if not os.path.exists(yearpath):
-            os.mkdir(yearpath)
 
         argsmerge = ['cdo', 'mergetime', os.path.join(daypath, "*Np.%04d*" % y), yearfile]
         try:
@@ -114,3 +118,73 @@ for y in years:
                 os.remove(yearfile)
         #cmd = " ".join(argsmerge)
         #os.system(cmd)
+
+## lets work on the surface directory
+if not os.path.exists(surface_raw_dir):
+    sys.exit()
+
+if rank == 0:
+    surface_files = []
+    for root, dirs, files in os.walk(surface_raw_dir):
+        surface_files += [[os.path.join(root, f), f] for f in files if f[-3:] == 'nc4']
+    surface_files = numpy.array_split(surface_files, size)
+else:
+    surface_files = None
+
+surface_files = comm.scatter(surface_files)
+
+for fpath, f in surface_files:
+    remapdir = os.path.join(base_dir, "6hour", "remap_surface")
+    remapfile = os.path.join(remapdir, f.replace(".nc4", "_1.0x1.25.nc4"))
+    if not os.path.exists(remapfile):
+        tempfile = "temp" + f 
+        argsvars = ['cdo', 'selname,%s' % ",".join(surface_vars), fpath, tempfile]
+        subprocess.check_call(argsvars)
+        args = ['cdo', 'remapbil,%s' % remap, tempfile, remapfile]
+        subprocess.check_call(args)
+        os.remove(tempfile)
+
+    for v in surface_vars:
+        vardir = os.path.join(base_dir, "6hour", v)
+        varfile = os.path.join(vardir, os.path.basename(remapfile).replace(".nc4", "_%s.nc4" % v))
+        if not os.path.exists(varfile):
+            if not os.path.exists(vardir):
+                os.mkdir(vardir)
+            argsvar = ['cdo', 'selname,%s' % v , remapfile, varfile] 
+            try:
+                subprocess.check_call(argsvar)
+            except Exception as err:
+                print err
+                os.remove(remapfile)
+                continue
+            dailyvardir = os.path.join(base_dir, "daily", v)
+            dailyvarfile = os.path.join(dailyvardir, os.path.basename(varfile))
+            if not os.path.exists(dailyvarfile):
+                if not os.path.exists(dailyvardir):
+                    os.mkdir(dailyvardir)
+                argsmean = ['cdo', 'daymean', varfile, dailyvarfile]
+                try:
+                    subprocess.check_call(argsmean)
+                except Exception as err:
+                    print err
+                    os.remove(varfile)
+                    continue
+
+for y in years:
+    for var in surface_vars:
+        daypath = os.path.join(base_dir, "daily", var)
+        yearpath = os.path.join(base_dir, "daily_merged", var)
+        yearfile = os.path.join(yearpath, "MERRA2.instD_3d_ana_Nx.%04d_1.0x1.25_%s.nc4" % (y, var))
+        if not os.path.exists(yearpath):
+            os.mkdir(yearpath)
+        if (os.path.exists(yearfile)): # and (os.path.getsize(yearfile) != 0):
+#            os.remove(yearfile) 
+            continue
+
+        argsmerge = ['cdo', 'mergetime', os.path.join(daypath, "*Nx.%04d*" % y), yearfile]
+        try:
+            subprocess.check_call(argsmerge)
+        except Exception as err:
+            print err
+            if os.path.exists(yearfile):
+                os.remove(yearfile)
