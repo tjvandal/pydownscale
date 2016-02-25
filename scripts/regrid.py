558b5366c4fd97feed03ac764af47861c35f8d87
tjvandal <tvandal20@gmail.com>
2016-02-25 15:39:23 -0500

    most recent regridding

--- scripts/regrid.py
+++ scripts/regrid.py
@@ -3,20 +3,25 @@ import sys
 import subprocess
 
 remapfile = os.path.join(os.path.dirname(__file__), "merra_remap_1.0x1.25")
-basencdir = "/gss_gpfs_scratch/vandal.t/cmip5/historical/atm/mon/ccsm/"
+basencdir = "/gss_gpfs_scratch/vandal.t/cmip5/historical/atm/day/ccsm4/"
 rawdir = os.path.join(basencdir, "raw")
 remapdir = os.path.join(basencdir, "remap")
 
 subdirs = os.listdir(rawdir)
 
 for d in subdirs:
+    print "subdirectory", d
     subpath = os.path.join(rawdir, d)
     varpath = os.path.join(remapdir, d)
+    if not os.path.exists(varpath):
+        os.mkdir(varpath)
+    if not os.path.isdir(subpath):
+        continue
     for f in os.listdir(subpath):
         if f[-3:] in (".nc", "nc4"):
             rawpath = os.path.join(subpath, f)
             remappath = os.path.join(varpath, f)
             args = ['cdo', 'remapbil,%s' % remapfile, rawpath, remappath]
-            print args
-            subprocess.check_call(args)
-
+            print " ".join(args)
+            if not os.path.exists(remappath):
+                subprocess.check_call(args)
