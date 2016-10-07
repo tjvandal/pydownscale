03e60a221535e96e9a8d1470a4b2bd3c2bf49c84
tjvandal <tvandal20@gmail.com>
2016-10-06 22:22:13 -0400

    move function into main()

--- mpi/scripts/mpi_asd.py
+++ mpi/scripts/mpi_asd.py
@@ -10,79 +10,85 @@ sys.path.insert(0, pydownscale_dir)
 
 from mpi4py import MPI
 import numpy
+import pickle
+import xarray
+
 from pydownscale.data import DownscaleData
 from pydownscale import config
 from pydownscale import utils
-import xarray
 from pydownscale.utils import LogTransform, BoxcoxTransform
+from pydownscale.downscale import ASD
+from pydownscale.stepwise_regression import BackwardStepwiseRegression
+from pydownscale.bma import BMA
+
+from sklearn.decomposition import PCA
+from sklearn import svm
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LassoLarsCV,LinearRegression, LogisticRegression
 from sklearn import linear_model
-from pydownscale.downscale import ASD
-import pickle
-from pydownscale.stepwise_regression import BackwardStepwiseRegression
-from pydownscale.bma import BMA
-from sklearn.decomposition import PCA
-from sklearn import svm
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
-print "Size: %i, Rank: %i" % (size, rank)
+def main():
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    print "Size: %i, Rank: %i" % (size, rank)
 
-size_per_job = 40
-groupworld = comm.Get_group()
-num_groups = max(int(size/size_per_job), 1)
-jobranks = [i for i in range(size)]
-jobranks = numpy.array_split(jobranks, num_groups)
+    size_per_job = 40
+    groupworld = comm.Get_group()
+    num_groups = max(int(size/size_per_job), 1)
+    jobranks = [i for i in range(size)]
+    jobranks = numpy.array_split(jobranks, num_groups)
 
-# Build groups and communicators 
-groups = [groupworld.Range_incl([(sub[0],sub[-1],1),]) for sub in jobranks]
-groupcomms =[comm.Create(g) for g in groups]
+    # Build groups and communicators 
+    groups = [groupworld.Range_incl([(sub[0],sub[-1],1),]) for sub in jobranks]
+    groupcomms =[comm.Create(g) for g in groups]
 
 
-for j, sub in enumerate(jobranks):
-    if rank in sub:
-        mygroupidx = j
-        mygroup = groups[j]
-        rootrank = 0
-        mygroupcomm = groupcomms[j]
-        mygrouprank = mygroupcomm.Get_rank()
-        break
+    for j, sub in enumerate(jobranks):
+        if rank in sub:
+            mygroupidx = j
+            mygroup = groups[j]
+            rootrank = 0
+            mygroupcomm = groupcomms[j]
+            mygrouprank = mygroupcomm.Get_rank()
+            break
 
-if mygrouprank == 0:
-    datafile = '/gss_gpfs_scratch/vandal.t/experiments/DownscaleData/newengland_D_12781_8835.pkl'
-    data = pickle.load(open(datafile, 'r'))
-    jobs = []
-    seasons = ['SON', 'DJF', 'MAM', 'JJA']
-    for seas in seasons:
-        jobs += [
-             #["ELNET", seas,  ASD(data, nearest_neighbor=False, 
-             #         model=linear_model.ElasticNetCV(l1_ratio=[0.1,0.25,0.5,0.75,0.9,1.], n_alphas=10),
-             #          season=seas, xtransform=StandardScaler(), cond_thres=10.,
-             #                     conditional=LogisticRegression("l1", C=0.1),
-             #         ytransform=utils.RootTransform(0.24), num_proc=size_per_job)], 
-            #["PCAOLS", seas,  ASD(data, nearest_neighbor=False, model=LinearRegression(),season=seas, 
-            #             xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
-            #                      num_proc=size_per_job, conditional=LogisticRegression("l2", C=10.),
-            #                      cond_thres=10.)],
-             # ["PCASVRRoot", seas,  ASD(data, nearest_neighbor=False, model=svm.LinearSVR(),season=seas, 
-             #            xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
-             #            num_proc=size_per_job, conditional=svm.SVC(kernel='linear'),
-             #                           cond_thres=10.)],
-              ["PCASVRLogistic", seas,  ASD(data, nearest_neighbor=False, model=svm.LinearSVR(),season=seas, 
-                         xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
-                         num_proc=size_per_job, conditional=LogisticRegression("l2", C=10.), cond_thres=10.)]
-            ]
-    jobsplit = numpy.array_split(jobs, num_groups)
-    myjobs = jobsplit[mygroupidx]
-    for key, seas, model in myjobs:
-        try:
-            print "training model %s for season %s" % (key, seas)
-            model.train()
-            predicted = model.predict(test_set=True)
-            pickle.dump(model, open("/gss_gpfs_scratch/vandal.t/experiments/asd_pkl/%s_%s.pkl" % (key, seas), "w"))
-            predicted.to_netcdf("/gss_gpfs_scratch/vandal.t/experiments/asd_nc/%s_D_%s.nc" % (key, seas))
-            del model
-        except Exception as err:
-            print key, err
+    if mygrouprank == 0:
+        datafile = '/gss_gpfs_scratch/vandal.t/experiments/DownscaleData/newengland_D_12781_8835.pkl'
+        data = pickle.load(open(datafile, 'r'))
+        jobs = []
+        seasons = ['SON', 'DJF', 'MAM', 'JJA']
+        for seas in seasons:
+            jobs += [
+                 #["ELNET", seas,  ASD(data, nearest_neighbor=False, 
+                 #         model=linear_model.ElasticNetCV(l1_ratio=[0.1,0.25,0.5,0.75,0.9,1.], n_alphas=10),
+                 #          season=seas, xtransform=StandardScaler(), cond_thres=10.,
+                 #                     conditional=LogisticRegression("l1", C=0.1),
+                 #         ytransform=utils.RootTransform(0.24), num_proc=size_per_job)], 
+                #["PCAOLS", seas,  ASD(data, nearest_neighbor=False, model=LinearRegression(),season=seas, 
+                #             xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
+                #                      num_proc=size_per_job, conditional=LogisticRegression("l2", C=10.),
+                #                      cond_thres=10.)],
+                 # ["PCASVRRoot", seas,  ASD(data, nearest_neighbor=False, model=svm.LinearSVR(),season=seas, 
+                 #            xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
+                 #            num_proc=size_per_job, conditional=svm.SVC(kernel='linear'),
+                 #                           cond_thres=10.)],
+                  ["PCASVRLogistic", seas,  ASD(data, nearest_neighbor=False, model=svm.LinearSVR(),season=seas, 
+                             xtransform=PCA(n_components=0.98), ytransform=utils.RootTransform(0.25),
+                             num_proc=size_per_job, conditional=LogisticRegression("l2", C=10.), cond_thres=10.)]
+                ]
+        jobsplit = numpy.array_split(jobs, num_groups)
+        myjobs = jobsplit[mygroupidx]
+        for key, seas, model in myjobs:
+            try:
+                print "training model %s for season %s" % (key, seas)
+                model.train()
+                predicted = model.predict(test_set=True)
+                pickle.dump(model, open("/gss_gpfs_scratch/vandal.t/experiments/asd_pkl/%s_%s.pkl" % (key, seas), "w"))
+                predicted.to_netcdf("/gss_gpfs_scratch/vandal.t/experiments/asd_nc/%s_D_%s.nc" % (key, seas))
+                del model
+            except Exception as err:
+                print key, err
+
+if __name__ == "__main__":
+    main()
